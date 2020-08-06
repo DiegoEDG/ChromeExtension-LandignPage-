37dc5b61879c3aba3b53a2404f7cde56f3b3ab0d
Diego <1.5.dg.diego@gmail.com>
2020-08-06 11:32:45 -0500

    Se añadio la caracteristica de responsive design

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let menu=document.getElementById('menu')
 let btn_menu=document.getElementById('button-menu')
 let btn_close=document.getElementById('button-close')
-let header=document.getElementById('header')
+let logo=document.getElementById('logo')
 let question_button1=document.getElementById('question-button-1')
 let question_button2=document.getElementById('question-button-2')
 let question_button3=document.getElementById('question-button-3')
@@ -13,11 +13,13 @@ let answer4=document.getElementById('answer-4')
 
 btn_menu.addEventListener("click",function() {
   menu.classList.toggle("hide")
-  header.classList.toggle("hide")
+  logo.classList.toggle("hide")
+  btn_menu.classList.toggle("hide")
 })
 btn_close.addEventListener("click",function() {
   menu.classList.toggle("hide")
-  header.classList.toggle("hide")
+  logo.classList.toggle("hide")
+  btn_menu.classList.toggle("hide")
 })
 question_button1.addEventListener("click",function() {
   answer1.classList.toggle("hide")
